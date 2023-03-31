d702c09b2b4020c22d9e32f9e0bd9f3270a9f48e
Quy <lequy332@gmail.com>
2023-03-31 08:59:31 +0700

    Add project files.

---
+++ BigSchool/Migrations/202303210334414_AddFollowingsTable.cs
@@ -0,0 +1,35 @@
+﻿namespace BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddFollowingsTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Followings",
+                c => new
+                    {
+                        FollowingId = c.String(nullable: false, maxLength: 128),
+                        Follower_Id = c.String(nullable: false, maxLength: 128),
+                        Followee_Id = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => t.FollowingId)
+                .ForeignKey("dbo.AspNetUsers", t => t.Follower_Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.Followee_Id)
+                .Index(t => t.Follower_Id)
+                .Index(t => t.Followee_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Followings", "Followee_Id", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Followings", "Follower_Id", "dbo.AspNetUsers");
+            DropIndex("dbo.Followings", new[] { "Followee_Id" });
+            DropIndex("dbo.Followings", new[] { "Follower_Id" });
+            DropTable("dbo.Followings");
+        }
+    }
+}
---
+++ BigSchool/Migrations/202303210416579_AddFollowingsTable.cs
@@ -0,0 +1,30 @@
+﻿namespace BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddFollowingsTable : DbMigration
+    {
+        public override void Up()
+        {
+            RenameColumn(table: "dbo.Followings", name: "Follower_Id", newName: "FollowerId");
+            RenameColumn(table: "dbo.Followings", name: "Followee_Id", newName: "FolloweeId");
+            RenameIndex(table: "dbo.Followings", name: "IX_Follower_Id", newName: "IX_FollowerId");
+            RenameIndex(table: "dbo.Followings", name: "IX_Followee_Id", newName: "IX_FolloweeId");
+            DropPrimaryKey("dbo.Followings");
+            AddPrimaryKey("dbo.Followings", new[] { "FollowerId", "FolloweeId" });
+            DropColumn("dbo.Followings", "FollowingId");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Followings", "FollowingId", c => c.String(nullable: false, maxLength: 128));
+            DropPrimaryKey("dbo.Followings");
+            AddPrimaryKey("dbo.Followings", "FollowingId");
+            RenameIndex(table: "dbo.Followings", name: "IX_FolloweeId", newName: "IX_Followee_Id");
+            RenameIndex(table: "dbo.Followings", name: "IX_FollowerId", newName: "IX_Follower_Id");
+            RenameColumn(table: "dbo.Followings", name: "FolloweeId", newName: "Followee_Id");
+            RenameColumn(table: "dbo.Followings", name: "FollowerId", newName: "Follower_Id");
+        }
+    }
+}
